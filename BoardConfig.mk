673bd811b2094ae8e82115c0840fc9bc5d98dc60
Jamey Hicks <jamey.hicks@nokia.com>
2012-10-29 15:55:48 -0400

    initial zedboard xylon configuration

---
+++ BoardConfig.mk
@@ -0,0 +1,39 @@
+# config.mk
+#
+# Product-specific compile-time definitions.
+#
+
+# The generic product target doesn't have any hardware-specific pieces.
+TARGET_NO_BOOTLOADER := true
+TARGET_NO_KERNEL := true
+
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+TARGET_CPU_SMP := true
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Build OpenGLES emulation guest and host libraries
+BUILD_EMULATOR_OPENGL := false
+
+# Build and enable the OpenGL ES View renderer. When running on the emulator,
+# the GLES renderer disables itself if host GL acceleration isn't available.
+USE_OPENGL_RENDERER := false
+
+
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_USERIMAGES_USE_EXT4 := true
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 268435456
+#BOARD_SYSTEMIMAGE_PARTITION_SIZE := 16777216
+BOARD_USERDATAIMAGE_PARTITION_SIZE := 536870912
+BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
+BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_FLASH_BLOCK_SIZE := 4096
+
+SMALLER_FONT_FOOTPRINT := true
+MINIMAL_FONT_FOOTPRINT := true
+# Some framework code requires this to enable BT
+BOARD_HAVE_BLUETOOTH := false
+
+WPA_SUPPLICANT_VERSION := VER_0_8_X
---
+++ zedxylon.mk
@@ -0,0 +1,30 @@
+# Copyright (C) 2012 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+$(call inherit-product, build/target/product/mini.mk)
+
+PRODUCT_PACKAGES += gralloc.zedboard hwcomposer.zedboard
+## PRODUCT_PACKAGES += busybox
+
+PRODUCT_NAME := zedxylon
+TARGET_BOOTLOADER_BOARD_NAME := zedboard
+TARGET_BOARD_PLATFORM := xylonfb
+
+PRODUCT_DEVICE := zedxylon
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Mini for zynq zedboard xylonfb
+
+# default is nosdcard, S/W button enabled in resource
+DEVICE_PACKAGE_OVERLAYS := device/generic/armv7-a-neon/overlay
+PRODUCT_CHARACTERISTICS := nosdcard

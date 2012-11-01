b4a9235f22b10eef791056f49587a0f8c6808147
Jamey Hicks <jamey.hicks@nokia.com>
2012-11-01 10:49:15 -0400

    zedfull -> zedboard

--- zedboard.mk
+++ zedboard.mk
@@ -1,5 +1,4 @@
-#
-# Copyright (C) 2011 The Android Open-Source Project
+# Copyright (C) 2012 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,7 +11,20 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
 
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/zedboard.mk
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_PACKAGES += gralloc.zedboard hwcomposer.zedboard
+## PRODUCT_PACKAGES += busybox
+
+PRODUCT_NAME := zedboard
+TARGET_BOOTLOADER_BOARD_NAME := zedboard
+TARGET_BOARD_PLATFORM := zynq
+
+PRODUCT_DEVICE := zedboard
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Mini for zynq zedboard
+
+# default is nosdcard, S/W button enabled in resource
+DEVICE_PACKAGE_OVERLAYS := device/generic/armv7-a-neon/overlay
+PRODUCT_CHARACTERISTICS := nosdcard
